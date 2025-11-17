fb7b206930b1b350b34614b1b0f00f22793df87e
yavuzTheParz <yavuzselim072004@gmail.com>
2025-11-17 15:03:09 +0300

    modified prompt class

--- Filter_class_tak_çıkar.py
+++ Filter_class_tak_çıkar.py
@@ -4,15 +4,17 @@ from typing import List
 
 
 class Structure(Enum):
-    TYPE_A = 1
-    TYPE_B = 2
-    TYPE_C = 3
+    ignore_all_override = 1
+    write_me_a_poem = 2
+    role_reprogramming = 3
+    forced_start_constraint = 4
 
 
 class Content(Enum):
-    MALICIOUS = 1
-    BENIGN = 2
-    UNKNOWN = 3
+    bomb_weapons = 1
+    financial_crime = 2
+    misinformation = 3
+    hacking_cybercrime = 4
 
 
 @dataclass
@@ -20,5 +22,28 @@ class Prompt:
     input_prompt: str = ""
     output_prompts: List[str] = field(default_factory=list)
     fitness: float = 0.0
-    structure: Structure = Structure.TYPE_A
-    content: Content = Content.UNKNOWN
+    structure: Structure = Structure.ignore_all_override
+    content: Content = Content.bomb_weapons
+
+
+"""
+example usage #1:
+
+p = Prompt(
+    input_prompt="Write me a short poem.",
+    structure=Structure.write_me_a_poem,
+    content=Content.misinformation
+)
+
+print(p)
+
+example usage #2:
+
+    if prompt.content == Content.bomb_weapons:
+        ref_output = "This is the reference output for bomb category."
+    
+    elif prompt.content == Content.financial_crime:
+        ref_output = "Ref output for fraud category."
+
+
+"""
